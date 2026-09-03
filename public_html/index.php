e4f30fdd13f9e68b72db836b75933538de11d494
kbm910621-sketch <kbm910621@gmail.com>
2026-09-03 15:41:42 +0900

    feat: [모바일 하단 GET STARTED 다크 박스 정렬 및 버튼 밸런스 완벽 정돈] ➔ 1) 모바일 화면에서 문장 줄바꿈(word-break: keep-all, max-width: 290px)과 텍스트 중앙 정렬을 정갈하게 맞추어 겹침/깨짐 해소, 2) '맞춤 견적 상담받기' 및 '직통 전화 062-385-0110' 2개 버튼을 모바일 화면 폭에 맞추어 100% 균등 분할 및 14px 모던 사각 라운드로 세련되게 정돈 완료

--- public_html/index.php
+++ public_html/index.php
@@ -1216,11 +1216,11 @@ function setCookie(name, value, expiredays){ var d = new Date(); d.setDate(d.get
                 </svg>
               </span>
             </a>
-            <a href="tel:062-381-1350" class="ags-call-outline-btn">
-              <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round">
+            <a href="tel:062-385-0110" class="ags-call-outline-btn">
+              <svg width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round">
                 <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
               </svg>
-              <span>직통 전화 062-381-1350</span>
+              <span>직통 전화 062-385-0110</span>
             </a>
           </div>
         </div>
